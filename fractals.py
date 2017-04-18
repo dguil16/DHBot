bc68161e31589017b9b120476c70d54dce93b550
Xorin <zendevar@gmail.com>
2017-04-18 15:59:18 -0500

    Fixed

--- fractals.py
+++ fractals.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import fractals
 import json
 import os
+import requests
 import sys
 from os.path import getmtime
 import logging
@@ -13,17 +14,17 @@ import trivia
 
 import discord
 
-async def enroll(client, messagae, Discord_ID, API_key):
+async def enroll(client, message, Discord_ID, API_key):
 	try:
 		response = requests.get("https://api.guildwars2.com/v2/tokeninfo?access_token={}".format(API_key))
-		API_key_info = json.loads(response)
+		API_key_info = json.loads(response.text)
 	except:
 		await client.send_message(message.channel, "There seems to be an error with your API key. Please ensure it is correct and try again.")
-	if set(API_key_info["permissions"]) not >= {"builds", "progressions", "account", "characters"}:
+	if set(API_key_info["permissions"]).intersection(set(["builds", "progression", "account", "characters"])) != {"builds", "progression", "account", "characters"}:
 		await client.send_message(message.channel, "This API key does not have the proper permissions. Please ensure the key has the Account, Progression, Builds, and Characters permissions.")
 	else:
 		try:
-			with open("fractal_users.txt", 'r') a f:
+			with open("fractal_users.txt", 'r') as f:
 				fractal_users = json.load(f)
 		except:
 			fractal_users = {}
@@ -41,12 +42,14 @@ async def enroll(client, messagae, Discord_ID, API_key):
 
 async def unenroll(client, message, Discord_ID):
 	try: 
-		with open("fractal_users.txt", 'r') a f:
+		with open("fractal_users.txt", 'r') as f:
 			fractal_users = json.load(f)
 	except:
 		fractal_users = {}
 	try:
 		del fractal_users[Discord_ID]
+		with open("fractal_users.txt", 'w') as f:
+			f.write(str(json.dumps(fractal_users)))
 		await client.send_message(message.channel, message.author.name + " was removed from the Fractal list.")
 	except:
 		await client.send_message(message.channel, "There was an error with your request. Likely, you are not currently a member of the Fractal list.")
